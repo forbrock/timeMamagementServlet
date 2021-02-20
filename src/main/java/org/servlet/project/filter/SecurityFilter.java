fff3007ec8bb97fa17ed9091dedc80209bcadbe8
forbrock <badgeon@gmail.com>
2021-02-20 12:41:51 +0200

    login page fixed

--- src/main/java/org/servlet/project/filter/SecurityFilter.java
+++ src/main/java/org/servlet/project/filter/SecurityFilter.java
@@ -7,12 +7,16 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 @WebFilter("/app/*")
 public class SecurityFilter implements Filter {
     private SecurityService securityService;
+    private final List<String> unauthorizedPaths = Arrays.asList("/login", "/registration");
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -20,24 +24,27 @@ public class SecurityFilter implements Filter {
     }
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        String uri = request.getRequestURI().replaceFirst(request.getContextPath() + "/app", "");
+        String uri = request.getRequestURI()
+                .replaceFirst(request.getContextPath() + "/app", "");
         User loggedUser = securityService.getLoggedUser(request.getSession());
 
         if (Objects.isNull(loggedUser)) {
-            response.sendRedirect(request.getContextPath() + request.getServletPath() + "/login");
-            return;
+            if (unauthorizedPaths.contains(uri)) {
+                chain.doFilter(request, response);
+                return;
+            } else {
+                response.sendRedirect(
+                        request.getContextPath() + request.getServletPath() + "/login");
+                return;
+            }
         }
 
-/*
-        if (servletPath.equals("/login")) {
-            chain.doFilter(request, response);
-            return;
-        }
-*/
         chain.doFilter(request, response);
     }
 
